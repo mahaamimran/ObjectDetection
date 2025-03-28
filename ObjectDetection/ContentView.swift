0e584d420cc9b193482c75e1b6b82e19fe0f6586
mahaamimran <mahaamimran@gmail.com>
2025-03-28 20:04:57 +0500

    Initial Commit

---
+++ ObjectDetection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObjectDetection
+//
+//  Created by Maham Imran on 28/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObjectDetection/ObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectDetectionApp.swift
+//  ObjectDetection
+//
+//  Created by Maham Imran on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
